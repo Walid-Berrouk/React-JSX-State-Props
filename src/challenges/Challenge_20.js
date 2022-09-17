8fa2a1fe502dd3fd273a1c580b6566ec3d11ca7f
Walid Berrouk <jw_berrouk@esi.dz>
2022-09-17 14:16:35 +0100

    Adding a map in Challenge 20

--- src/challenges/Challenge_20.js
+++ src/challenges/Challenge_20.js
@@ -3,6 +3,21 @@ import Card from './Challenge_20/Card'
 
 function Challenge_20() {
 
+    const cards = [
+        <p className="card-text">Some quick description for you dish.</p>,
+        <>
+            <p className="card-text">Some description to your dish and</p>
+            <a href="#" className="card-link">A link to it</a>
+        </>,
+        <>
+            <p className="card-text">Some description to your dish and</p>
+            <a href="#" className="card-link">A link to it</a>
+            <a href="#" className="card-link">Another link to it</a>
+            <a href="#" className="card-link">A whole bunch of links to it</a>
+        </>,
+        <p className="card-text">Anything !</p>
+    ]
+
 
   return (
     <div>
@@ -15,22 +30,13 @@ function Challenge_20() {
             <div className='border p-20 flex justify-center'>
                 <h1 className='mb-5'>Let's Discover the power of having children !</h1>
                 <div className='flex'>
-                    <Card>
-                        <p className="card-text">Some quick description for you dish.</p>
-                    </Card>
-                    <Card>
-                        <p className="card-text">Some description to your dish and</p>
-                        <a href="#" className="card-link">A link to it</a>
-                    </Card>
-                    <Card>
-                        <p className="card-text">Some description to your dish and</p>
-                        <a href="#" className="card-link">A link to it</a>
-                        <a href="#" className="card-link">Another link to it</a>
-                        <a href="#" className="card-link">A whole bunch of links to it</a>
-                    </Card>
-                    <Card>
-                        <p className="card-text">Anything !</p>
-                    </Card>
+                   {
+                    cards.map(card => (
+                        <Card>
+                            {card}
+                        </Card>
+                    ))
+                   }
 
                 </div>
             </div>
